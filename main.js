f6839d95f5c0e98db0cb43ee32977d69cb573117
bobboteck <bobboteck@gmail.com>
2016-11-01 15:03:09 +0100

    First commit of application

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log(__dirname);
+
+const {app, BrowserWindow} = require('electron');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 325
+        ,width: 300
+		,resizable: false
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+	//mainWindow.webContents.openDevTools()
+});
+
